0c9b60c65aa89ebc0fa2468b28f06ba1a07f97a2
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2026-05-11 17:02:38 +0300

    homework 7

--- homeworks/homework_7/solution.js
+++ homeworks/homework_7/solution.js
@@ -1,110 +1,94 @@
-const DATA_LENGTH_CAPACITY = 4;
-const ITEM_LENGTH_CAPACITY = 4;
+const HEADER_SIZE = 4;
+const ITEM_LEN_SIZE = 4;
+const ITEM_OFFSET_SIZE = 4;
 
 class StringBuffer {
-  buffer;
+  #buffer;
+  #view;
+  #bytes;
+  #decoder = new TextDecoder();
 
   getBuffer() {
-    return this.buffer;
+    return this.#buffer;
   }
 
   encodeStrings(data) {
-    if (!Array.isArray(data) || data.length === 0) {
-      return null;
-    }
-    const encodedStrings = [];
+    if (!Array.isArray(data) || data.length === 0) return null;
 
-    let encoder = new TextEncoder();
-    for (let i = 0; i < data.length; i++) {
-      encodedStrings.push(encoder.encode(data[i]));
-    }
+    const encoder = new TextEncoder();
+    const encodedStrings = data.map((str) => encoder.encode(str));
 
-    const calucluatedDataCapacity = encodedStrings.reduce((acc, cur) => {
-      return (acc += cur.length);
-    }, 0);
-
+    const dataCapacity = encodedStrings.reduce(
+      (acc, cur) => acc + cur.length,
+      0,
+    );
     const totalCapacity =
-      DATA_LENGTH_CAPACITY +
-      ITEM_LENGTH_CAPACITY * data.length +
-      calucluatedDataCapacity;
+      HEADER_SIZE + ITEM_LEN_SIZE * data.length + dataCapacity;
 
-    const newBuffer = new ArrayBuffer(totalCapacity);
-    const view = new DataView(newBuffer);
+    this.#buffer = new ArrayBuffer(totalCapacity);
+    this.#view = new DataView(this.#buffer);
+    this.#bytes = new Uint8Array(this.#buffer);
 
     let offset = 0;
+    this.#view.setUint32(offset, data.length);
+    offset += HEADER_SIZE;
 
-    view.setUint32(0, data.length);
-    offset += DATA_LENGTH_CAPACITY;
-
-    for (let i = 0; i < encodedStrings.length; i++) {
-      view.setUint32(offset, encodedStrings[i].length);
-      offset += ITEM_LENGTH_CAPACITY;
-      const bytes = new Uint8Array(newBuffer);
-      bytes.set(encodedStrings[i], offset);
-
-      offset += encodedStrings[i].length;
+    for (const encoded of encodedStrings) {
+      this.#view.setUint32(offset, encoded.length);
+      offset += ITEM_LEN_SIZE;
+      this.#bytes.set(encoded, offset);
+      offset += encoded.length;
     }
 
-    this.buffer = newBuffer;
-  }
-
-  decodeStrings(buffer) {
-    let offset = 0;
-
-    const view = new DataView(this.buffer);
-    const totalStrings = view.getUint32(0);
-    offset += DATA_LENGTH_CAPACITY;
-    let decodedStrings = [];
-    const decoder = new TextDecoder();
-
-    for (let i = 0; i < totalStrings; i++) {
-      const view = new DataView(buffer);
-      const stringLength = view.getUint32(offset);
-
-      offset += ITEM_LENGTH_CAPACITY;
-
-      const stringBytes = new Uint8Array(buffer).subarray(
-        offset,
-        offset + stringLength,
-      );
-
-      decodedStrings.push(decoder.decode(stringBytes));
-      offset += stringLength;
-    }
-
-    return decodedStrings;
+    return this.#buffer;
   }
 
   at(index) {
-    if (!this.buffer) {
+    if (!this.#buffer) return undefined;
+
+    const totalStrings = this.#view.getUint32(0);
+    const normalizedIndex = index < 0 ? totalStrings + index : index;
+
+    if (normalizedIndex < 0 || normalizedIndex >= totalStrings) {
       return undefined;
     }
 
-    const view = new DataView(this.buffer);
+    let offset = HEADER_SIZE;
+
+    for (let i = 0; i < normalizedIndex; i++) {
+      const stringLength = this.#view.getUint32(offset);
+      offset += ITEM_LEN_SIZE + stringLength;
+    }
+
+    const targetLength = this.#view.getUint32(offset);
+    offset += ITEM_LEN_SIZE;
+
+    const stringBytes = this.#bytes.subarray(offset, offset + targetLength);
+    return this.#decoder.decode(stringBytes);
+  }
+
+  decodeStrings(buffer = this.#buffer) {
+    if (!buffer) return [];
+
+    const view = buffer === this.#buffer ? this.#view : new DataView(buffer);
+    const bytes =
+      buffer === this.#buffer ? this.#bytes : new Uint8Array(buffer);
+
     const totalStrings = view.getUint32(0);
+    let offset = HEADER_SIZE;
+    const result = new Array(totalStrings);
 
-    if (index < 0 || index >= totalStrings) {
-      return undefined;
-    }
-
-    let offset = DATA_LENGTH_CAPACITY;
-
-    for (let i = 0; i < index; i++) {
-      const view = new DataView(this.buffer);
+    for (let i = 0; i < totalStrings; i++) {
       const stringLength = view.getUint32(offset);
-      offset += stringLength + ITEM_LENGTH_CAPACITY;
+      offset += ITEM_LEN_SIZE;
+
+      const stringBytes = bytes.subarray(offset, offset + stringLength);
+      result[i] = this.#decoder.decode(stringBytes);
+
+      offset += stringLength;
     }
 
-    const targetLength = view.getUint32(offset);
-    offset += ITEM_LENGTH_CAPACITY;
-
-    const decoder = new TextDecoder();
-    const stringBytes = new Uint8Array(this.buffer).subarray(
-      offset,
-      offset + targetLength,
-    );
-
-    return decoder.decode(stringBytes);
+    return result;
   }
 }
 
@@ -114,3 +98,141 @@ console.log(test.at(0));
 console.log(test.at(1));
 
 console.log(test.decodeStrings(test.getBuffer()));
+
+class StringBufferV2 {
+  #buffer;
+  #view;
+  #bytes;
+  #decoder = new TextDecoder();
+
+  getBuffer() {
+    return this.#buffer;
+  }
+
+  encodeStrings(data) {
+    if (!Array.isArray(data) || data.length === 0) return null;
+
+    const encoder = new TextEncoder();
+    const encodedStrings = data.map((str) => encoder.encode(str));
+
+    const encodedStringsCapacity = encodedStrings.reduce(
+      (acc, cur) => acc + cur.length,
+      0,
+    );
+
+    let indexOffset = HEADER_SIZE;
+    let dataOffset =
+      HEADER_SIZE + (ITEM_LEN_SIZE + ITEM_OFFSET_SIZE) * data.length;
+
+    const totalCapacity = dataOffset + encodedStringsCapacity;
+
+    this.#buffer = new ArrayBuffer(totalCapacity);
+    this.#view = new DataView(this.#buffer);
+    this.#bytes = new Uint8Array(this.#buffer);
+
+    this.#view.setUint32(0, data.length);
+
+    for (let i = 0; i < encodedStrings.length; i++) {
+      this.#view.setUint32(indexOffset, encodedStrings[i].length);
+      this.#view.setUint32(indexOffset + ITEM_LEN_SIZE, dataOffset);
+      this.#bytes.set(encodedStrings[i], dataOffset);
+      indexOffset += ITEM_LEN_SIZE + ITEM_OFFSET_SIZE;
+      dataOffset += encodedStrings[i].length;
+    }
+
+    return this.#buffer;
+  }
+
+  at(index) {
+    if (!this.#buffer) return undefined;
+
+    const totalStrings = this.#view.getUint32(0);
+    const normalizedIndex = index < 0 ? totalStrings + index : index;
+
+    if (normalizedIndex < 0 || normalizedIndex >= totalStrings) {
+      return undefined;
+    }
+
+    const entryOffset =
+      HEADER_SIZE + (ITEM_LEN_SIZE + ITEM_OFFSET_SIZE) * normalizedIndex;
+
+    let stringLength = this.#view.getUint32(entryOffset);
+
+    let dataPointer = this.#view.getUint32(entryOffset + ITEM_LEN_SIZE);
+
+    const stringBytes = this.#bytes.subarray(
+      dataPointer,
+      dataPointer + stringLength,
+    );
+    return this.#decoder.decode(stringBytes);
+  }
+
+  decodeStrings(buffer = this.#buffer) {
+    if (!buffer) return [];
+
+    const view = buffer === this.#buffer ? this.#view : new DataView(buffer);
+    const bytes =
+      buffer === this.#buffer ? this.#bytes : new Uint8Array(buffer);
+
+    const totalStrings = view.getUint32(0);
+    const result = new Array(totalStrings);
+
+    for (let i = 0; i < totalStrings; i++) {
+      const entryOffset = HEADER_SIZE + (ITEM_LEN_SIZE + ITEM_OFFSET_SIZE) * i;
+      let stringLength = view.getUint32(entryOffset);
+      let dataPointer = view.getUint32(entryOffset + ITEM_LEN_SIZE);
+
+      const stringBytes = bytes.subarray(
+        dataPointer,
+        dataPointer + stringLength,
+      );
+      result[i] = this.#decoder.decode(stringBytes);
+    }
+
+    return result;
+  }
+
+  set(index, newValue) {
+    const encoder = new TextEncoder();
+    const encodedString = encoder.encode(newValue);
+
+    const totalStrings = this.#view.getUint32(0);
+    const normalizedIndex = index < 0 ? totalStrings + index : index;
+
+    if (normalizedIndex < 0 || normalizedIndex >= totalStrings) {
+      return undefined;
+    }
+
+    const entryOffset =
+      HEADER_SIZE + (ITEM_LEN_SIZE + ITEM_OFFSET_SIZE) * normalizedIndex;
+    const oldStringLength = this.#view.getUint32(entryOffset);
+
+    if (encodedString.length <= oldStringLength) {
+      const dataPointer = this.#view.getUint32(entryOffset + ITEM_LEN_SIZE);
+
+      this.#view.setUint32(entryOffset, encodedString.length);
+
+      this.#bytes.set(encodedString, dataPointer);
+    } else {
+      const currentStrings = this.decodeStrings();
+      currentStrings[normalizedIndex] = newValue;
+      this.encodeStrings(currentStrings);
+    }
+  }
+}
+
+const strings = ["hello", "мир", ""];
+
+const stringBufferV2 = new StringBufferV2();
+
+stringBufferV2.encodeStrings(strings);
+
+console.log(stringBufferV2.at(0)); // "hello"
+
+stringBufferV2.set(0, "Привет, ");
+
+console.log(stringBufferV2.at(0)); // "Привет, "
+
+stringBufferV2.set(-1, "!");
+
+console.log(stringBufferV2.decodeStrings(stringBufferV2.getBuffer())); // ["Привет, ", "мир", "!"]
